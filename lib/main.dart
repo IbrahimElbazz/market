3f0d43fadd8b2a64e936ec0cd54cd718ea667bf6
Ibrahim Elbaz <ibrahim.elbaz.1011@gmail.com>
2025-01-24 16:36:19 +0200

    add supabase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:market/core/constant/key.dart';
 import 'package:market/market.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
+  await Supabase.initialize(
+    url: KeySupabase.urlSupabase,
+    anonKey: KeySupabase.apiKeySupabase,
+  );
   runApp(const market());
 }
