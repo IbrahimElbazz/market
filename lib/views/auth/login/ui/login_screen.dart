8e90ead5d80fe8ae9b2636ebb50a5a9acb72bfb6
Ibrahim Elbaz <ibrahim.elbaz.1011@gmail.com>
2025-03-23 01:19:15 +0200

    .

---
+++ lib/views/auth/login/ui/login_screen.dart
@@ -0,0 +1,228 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:market/core/nav_bar/nav_bar.dart';
+import 'package:market/core/widgets/custom_text_field.dart';
+import 'package:market/core/widgets/gap.dart';
+import 'package:market/views/auth/login/logic/cubit/login_cubit.dart';
+import 'package:market/views/auth/login/logic/cubit/login_state.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return BlocConsumer<LoginCubit, LoginState>(
+      listener: (context, state) {
+        state.maybeWhen(
+          orElse: () {
+            return const SizedBox.shrink();
+          },
+          success: () {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => NavBar(),
+                ));
+          },
+          loading: () {
+            return showDialog(
+              context: context,
+              builder: (context) {
+                return const Center(
+                  child: CircularProgressIndicator(
+                    color: Colors.blue,
+                  ),
+                );
+              },
+            );
+          },
+          error: (errorMessage) {
+            Navigator.pop(context);
+            return ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                content: Center(child: Text(errorMessage)),
+                backgroundColor: Colors.blue,
+              ),
+            );
+          },
+        );
+      },
+      buildWhen: (previous, current) {
+        return current is Success || current is Error || current is Loading;
+      },
+      builder: (context, state) {
+        final cub = context.read<LoginCubit>();
+        return Scaffold(
+          backgroundColor: const Color.fromARGB(255, 240, 240, 240),
+          body: SafeArea(
+            child: SingleChildScrollView(
+              child: SizedBox(
+                width: 430.w,
+                child: Column(
+                  children: [
+                    const GapH(height: 50),
+                    Text(
+                      'Welcome to our market',
+                      style: TextStyle(
+                        fontSize: 20.sp,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const GapH(height: 50),
+                    Container(
+                      width: 380.w,
+                      height: 500.h,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      child: Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 16.w),
+                        child: Form(
+                          key: context.read<LoginCubit>().formKey,
+                          child: Column(
+                            children: [
+                              const GapH(height: 50),
+                              CustomTextField(
+                                hint: 'Email',
+                                controller:
+                                    context.read<LoginCubit>().emailController,
+                              ),
+                              const GapH(height: 20),
+                              CustomTextField(
+                                hint: 'Password',
+                                isPassword: cub.isPasswordHidden,
+                                controller: context
+                                    .read<LoginCubit>()p
+                                    .passwordController,
+                                iconButton: IconButton(
+                                  onPressed: () {
+                                    cub.togglePasswordVisibility();
+                                  },
+                                  icon: Icon(
+                                    cub.isPasswordHidden
+                                        ? Icons.visibility_off
+                                        : Icons.visibility,
+                                    color: cub.isPasswordHidden
+                                        ? Colors.grey
+                                        : Colors.blue,
+                                  ),
+                                ),
+                              ),
+                              const GapH(height: 10),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: [
+                                  Text(
+                                    'Forget password ?',
+                                    style: TextStyle(
+                                      color: Colors.blue,
+                                      fontSize: 14.sp,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              const GapH(
+                                height: 30,
+                              ),
+                              GestureDetector(
+                                onTap: () {
+                                  if (context
+                                      .read<LoginCubit>()
+                                      .formKey
+                                      .currentState!
+                                      .validate()) {
+                                    context.read<LoginCubit>().login();
+                                  }
+                                },
+                                child: Container(
+                                  height: 55.h,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(16),
+                                    color: Colors.blue,
+                                  ),
+                                  child: Center(
+                                    child: Text(
+                                      'Login',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 18.sp,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              const GapH(height: 20),
+                              GestureDetector(
+                                onTap: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) {
+                                        return NavBar();
+                                      },
+                                    ),
+                                  );
+                                },
+                                child: Container(
+                                  height: 55.h,
+                                  width: 50.w,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(50),
+                                    color: Colors.red.withOpacity(0.2),
+                                  ),
+                                  child: const Center(
+                                    child: Icon(
+                                      Icons.g_mobiledata,
+                                      color: Colors.red,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              const GapH(height: 20),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    'Do not have acc ?',
+                                    style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 14.sp,
+                                    ),
+                                  ),
+                                  const GapW(width: 5),
+                                  Text(
+                                    'Register',
+                                    style: TextStyle(
+                                      color: Colors.blue,
+                                      fontSize: 14.sp,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
